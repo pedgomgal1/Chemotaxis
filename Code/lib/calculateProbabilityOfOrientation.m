51bf830c72e7a0eef1bca3b4ae5072722489dd3e
pedgomgal1 <pgomez-ibis@us.es>
2023-01-19 15:46:19 +0000

    calculating transition matrix of larvae orientation (markov chain)

---
+++ Code/lib/calculateProbabilityOfOrientation.m
@@ -0,0 +1,21 @@
+function [matrixProbOrientation,transitionMatrixOrientation]=calculateProbabilityOfOrientation(orderedAllLarvOrientPerSec)
+
+    nOrientStages = 4; % left - rigth - top - bottom
+    matrixProbOrientation = zeros(nOrientStages,nOrientStages);
+    uniqLabels = unique(orderedAllLarvOrientPerSec(:,1));
+    for nLar = 1:length(uniqLabels)
+        idsLab = ismember(orderedAllLarvOrientPerSec(:,1),uniqLabels(nLar));
+        larvOrientPerSec = orderedAllLarvOrientPerSec(idsLab,3:end);
+        auxCellMatrix=mat2cell(larvOrientPerSec,ones(size(larvOrientPerSec,1),1) ,4);
+        auxChangPosition =cellfun(@(x,y) [find(x),find(y)],auxCellMatrix(1:end-1,:),auxCellMatrix(2:end,:),'UniformOutput',false);
+        for nT=1:size(auxChangPosition,1)
+            subIndAux = [auxChangPosition{nT}];
+            matrixProbOrientation(subIndAux(1),subIndAux(2))=matrixProbOrientation(subIndAux(1),subIndAux(2))+1;    
+        end
+    
+    end
+    
+    %TRANSITION MATRIX - MARKOV CHAIN
+    transitionMatrixOrientation = matrixProbOrientation./sum(matrixProbOrientation);
+
+end
