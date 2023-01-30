ca3f745c7d75bb2044a31eed9f7020a14b368fcc
pedgomgal1 <pgomez-ibis@us.es>
2023-01-30 19:49:24 +0000

    simplify due to leak of memory

--- Code/lib/calculateAverageSpeedAndAcceleration.m
+++ Code/lib/calculateAverageSpeedAndAcceleration.m
@@ -4,19 +4,6 @@ function [avgSpeedRoundT, stdSpeedRoundT, semSpeedRoundT,avgMeanSpeed,avgStdSpee
     speedFileRoundT = speedFile;
     speedFileRoundT(:,2) = round(speedFile(:,2));
     uniqRoundTime = unique(round(speedFile(:,2)));
-
-%     %Plot individual speeds
-
-%     uniqLabels = unique(speedFileRoundT(:,1));
-%     figure;
-%     hold on
-%     for nLab=1:length(uniqLabels)
-%         idsLab = ismember(speedFileRoundT(:,1),uniqLabels(nLab));
-%         auxSpeedFileRoundT= speedFileRoundT(idsLab,:);
-%         auxUniqRoundTime = unique(round(auxSpeedFileRoundT(:,2)));
-%         auxAvgSpeedRoundT=arrayfun(@(x) mean(auxSpeedFileRoundT(ismember(auxSpeedFileRoundT(:,2),x),3)),auxUniqRoundTime);
-%         plot(auxUniqRoundTime,auxAvgSpeedRoundT, 'LineWidth', 2);
-%     end
      
     uniqueLabels = unique(speedFileRoundT(:,1));
 
@@ -24,30 +11,21 @@ function [avgSpeedRoundT, stdSpeedRoundT, semSpeedRoundT,avgMeanSpeed,avgStdSpee
     accelerationFileRoundTSimplified = [];
     dt = 1; % sec
     for nLab = 1:length(uniqueLabels)
-        for nTimP = 1:length(uniqRoundTime)
-            
-            idsT = ismember(speedFileRoundT(:,2),uniqRoundTime(nTimP));
-            idsLabel = ismember(speedFileRoundT(:,1),uniqueLabels(nLab));
 
-            speedFileRoundTSimplified = [speedFileRoundTSimplified;[uniqueLabels(nLab) uniqRoundTime(nTimP) mean([speedFileRoundT(idsT & idsLabel,3)])]];
+        idsLab = ismember(speedFileRoundT(:,1),uniqueLabels(nLab));
+        auxSpeedFileRoundT= speedFileRoundT(idsLab,:);
+        auxUniqRoundTime = unique(round(auxSpeedFileRoundT(:,2)));
+        auxAvgSpeedRoundT=arrayfun(@(x) mean(auxSpeedFileRoundT(ismember(auxSpeedFileRoundT(:,2),x),3)),auxUniqRoundTime);
 
-        end
+        speedFileRoundTSimplified = [speedFileRoundTSimplified;[ones(size(auxAvgSpeedRoundT))*uniqueLabels(nLab),auxUniqRoundTime,auxAvgSpeedRoundT]];
 
-        [rows,~]=find(isnan(speedFileRoundTSimplified));
-        speedFileRoundTSimplified(rows,:)=[];
+        dV = gradient(auxAvgSpeedRoundT);
 
-        idsLabel2 = ismember(speedFileRoundTSimplified(:,1),uniqueLabels(nLab));
-        dV = gradient(speedFileRoundTSimplified(idsLabel2,3));
-
-        plot(speedFileRoundTSimplified(idsLabel2,2),dV,'-') 
-        hold on
-        accelerationFileRoundTSimplified = [accelerationFileRoundTSimplified; [speedFileRoundTSimplified(idsLabel2,1:2) dV]];
-
+        accelerationFileRoundTSimplified = [accelerationFileRoundTSimplified; [ones(size(auxAvgSpeedRoundT))*uniqueLabels(nLab),auxUniqRoundTime, dV]];
 
     end
 
 
-    
     avgSpeedRoundT=arrayfun(@(x) mean(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3)),uniqRoundTime);
     semSpeedRoundT=arrayfun(@(x) std(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3))/sqrt(sum(ismember(speedFileRoundTSimplified(:,2),x))),uniqRoundTime);
     stdSpeedRoundT=arrayfun(@(x) std(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3)),uniqRoundTime);
