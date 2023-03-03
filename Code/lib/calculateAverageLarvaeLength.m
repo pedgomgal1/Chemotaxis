c2b559629ba47e45eca1d0577ee1e6cc31fe7e6d
pedgomgal1 <pgomez-ibis@us.es>
2023-03-03 12:16:25 +0000

    run this function in GPU

--- Code/lib/calculateAverageLarvaeLength.m
+++ Code/lib/calculateAverageLarvaeLength.m
@@ -1,15 +1,26 @@
 function averageLarvaLength = calculateAverageLarvaeLength(dataSpine)
-    arraydist1 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,3),dataSpine(:,4),dataSpine(:,5),dataSpine(:,6));
-    arraydist2 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,5),dataSpine(:,6),dataSpine(:,7),dataSpine(:,8));
-    arraydist3 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,7),dataSpine(:,8),dataSpine(:,9),dataSpine(:,10));
-    arraydist4 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,9),dataSpine(:,10),dataSpine(:,11),dataSpine(:,12));
-    arraydist5 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,11),dataSpine(:,12),dataSpine(:,13),dataSpine(:,14));
-    arraydist6 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,13),dataSpine(:,14),dataSpine(:,15),dataSpine(:,16));
-    arraydist7 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,15),dataSpine(:,16),dataSpine(:,17),dataSpine(:,18));
-    arraydist8 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,17),dataSpine(:,18),dataSpine(:,19),dataSpine(:,20));
-    arraydist9 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,19),dataSpine(:,20),dataSpine(:,21),dataSpine(:,22));
-    arraydist10 = arrayfun(@(p1,p2,p3,p4) pdist2([p1 p2],[p3,p4]),dataSpine(:,21),dataSpine(:,22),dataSpine(:,23),dataSpine(:,24));
     
-    totalSum = arraydist1+arraydist2+arraydist3+arraydist4+arraydist5+arraydist6+arraydist7+arraydist8+arraydist9+arraydist10;
+    try
+        dataSpineGpu = gpuArray(dataSpine);
+    catch
+        dataSpineGpu=dataSpine; 
+    end
+
+    n = size(dataSpineGpu, 1);
+    totalSum = zeros(n, 1);
+    for i = 3:2:size(dataSpineGpu, 2)-3  
+        x1 = dataSpineGpu(:, i);
+        y1 = dataSpineGpu(:, i+1);
+        x2 = dataSpineGpu(:, i+2);
+        y2 = dataSpineGpu(:, i+3);
+        segmentDist = arrayfun(@(x_1,y_1,x_2,y_2) sqrt((x_2-x_1)^2 + (y_2-y_1)^2), x1,y1,x2,y2);
+        totalSum = totalSum + segmentDist;
+    end
     averageLarvaLength = mean(totalSum);
+
+    if isgpuarray(averageLarvaLength)
+        averageLarvaLength = gather(averageLarvaLength);
+    end
+
+
 end
