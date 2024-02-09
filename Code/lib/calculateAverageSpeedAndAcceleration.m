ec2c64bbd4dcb5dbc00a9eb314bb00326c12c9e2
pedgomgal1 <pgomez-ibis@us.es>
2024-02-09 14:00:42 +0100

    simplyfing discretization speedRoundT

--- Code/lib/calculateAverageSpeedAndAcceleration.m
+++ Code/lib/calculateAverageSpeedAndAcceleration.m
@@ -1,34 +1,30 @@
 function [avgSpeedRoundT, stdSpeedRoundT, semSpeedRoundT,avgMeanSpeed,avgStdSpeed,avgSemSpeed,avgAccelerationRoundT, stdAccelerationRoundT, semAccelerationRoundT,avgMeanAcceleration,avgStdAcceleration,avgSemAcceleration,speedFileRoundTSimplified,accelerationFileRoundTSimplified] = calculateAverageSpeedAndAcceleration(speedFile)
 
     %Only measure the time intervals when the larva is not stacked. 
-    speedFileRoundT = speedFile;
-    speedFileRoundT(:,2) = round(speedFile(:,2));
-    uniqRoundTime = unique(round(speedFile(:,2)));
-     
+    %Round to discrete timepoints
+    %considering id in col 1, time in col 2 and property in col 3
+    speedFileRoundT = roundToDiscreteTimePoints (speedFile);
+
+    uniqRoundTime = unique(speedFileRoundT(:,2));
     uniqueLabels = unique(speedFileRoundT(:,1));
 
-    speedFileRoundTSimplified = [];
     accelerationFileRoundTSimplified = [];
-    dt = 1; % sec
     for nLab = 1:length(uniqueLabels)
 
         idsLab = ismember(speedFileRoundT(:,1),uniqueLabels(nLab));
         auxSpeedFileRoundT= speedFileRoundT(idsLab,:);
         auxUniqRoundTime = unique(round(auxSpeedFileRoundT(:,2)));
-        auxAvgSpeedRoundT=arrayfun(@(x) mean(auxSpeedFileRoundT(ismember(auxSpeedFileRoundT(:,2),x),3)),auxUniqRoundTime);
 
-        speedFileRoundTSimplified = [speedFileRoundTSimplified;[ones(size(auxAvgSpeedRoundT))*uniqueLabels(nLab),auxUniqRoundTime,auxAvgSpeedRoundT]];
+        dV = gradient(auxSpeedFileRoundT(:,3));
 
-        dV = gradient(auxAvgSpeedRoundT);
-
-        accelerationFileRoundTSimplified = [accelerationFileRoundTSimplified; [ones(size(auxAvgSpeedRoundT))*uniqueLabels(nLab),auxUniqRoundTime, dV]];
+        accelerationFileRoundTSimplified = [accelerationFileRoundTSimplified; [ones(sum(idsLab),1)*uniqueLabels(nLab),auxUniqRoundTime, dV]];
 
     end
 
 
-    avgSpeedRoundT=arrayfun(@(x) mean(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3)),uniqRoundTime);
-    semSpeedRoundT=arrayfun(@(x) std(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3))/sqrt(sum(ismember(speedFileRoundTSimplified(:,2),x))),uniqRoundTime);
-    stdSpeedRoundT=arrayfun(@(x) std(speedFileRoundTSimplified(ismember(speedFileRoundTSimplified(:,2),x),3)),uniqRoundTime);
+    avgSpeedRoundT=arrayfun(@(x) mean(speedFileRoundT(ismember(speedFileRoundT(:,2),x),3)),uniqRoundTime);
+    semSpeedRoundT=arrayfun(@(x) std(speedFileRoundT(ismember(speedFileRoundT(:,2),x),3))/sqrt(sum(ismember(speedFileRoundT(:,2),x))),uniqRoundTime);
+    stdSpeedRoundT=arrayfun(@(x) std(speedFileRoundT(ismember(speedFileRoundT(:,2),x),3)),uniqRoundTime);
 
 
     avgAccelerationRoundT=arrayfun(@(x) mean(accelerationFileRoundTSimplified(ismember(accelerationFileRoundTSimplified(:,2),x),3)),uniqRoundTime);
