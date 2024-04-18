1323d2d0c76937673efbd88c7f71e060b5b5ca3f
pedgomgal1 <pgomez-ibis@us.es>
2024-04-18 19:13:10 +0200

    organize plotBoxChart by separating chemotaxis and free nav experiments

--- Code/lib/plotGraphs/plotBoxChart.m
+++ Code/lib/plotGraphs/plotBoxChart.m
@@ -5,12 +5,14 @@ function plotBoxChart(Chemotaxis_data,FreeNav_data, colors, category,fontSizeFig
 grouped_data = [Chemotaxis_data,FreeNav_data];
 colors = [colors;colors];
 %indexes
-idx = [1,4,2,5,3,6]; 
+idx = [1,2,3,4,5,6]; 
 hold on;
 
 for i = 1:numel(grouped_data)
     % Plot boxplot
-    boxplot(grouped_data{idx(i)}, 'Positions', i, 'Widths', 0.4, 'Colors', [0 0 0]);
+    i2=i;
+    if i>3, i2=i+1; end
+    boxplot(grouped_data{idx(i)}, 'Positions', i2, 'Widths', 0.4, 'Colors', [0 0 0]);
 end
 % Set boxchart colors
 h = findobj(gca, 'Tag', 'Box');
@@ -19,20 +21,23 @@ for i = 1:numel(h)
     patch(get(h(i), 'XData'), get(h(i), 'YData'), box_color, 'FaceAlpha', 0.25, 'EdgeColor', 'k');
 end
 
-
 for i = 1:numel(grouped_data)
     % Plot individual data points as small circles
-    s=swarmchart(i*ones(size(grouped_data{idx(i)})), grouped_data{idx(i)}, 10, [colors(idx(i),:)], 'filled', 'MarkerEdgeColor', 'k');
+    i2=i;
+    if i>3, i2=i+1; end
+    s=swarmchart(i2*ones(size(grouped_data{idx(i)})), grouped_data{idx(i)}, 10, [colors(idx(i),:)], 'filled', 'MarkerEdgeColor', 'k');
     s.XJitterWidth = 0.25;
 end
 
 
 % Set labels and title
-set(gca, 'XTick', 1:numel(grouped_data), 'XTickLabel', {'Chemotaxis','Free Nav.', 'Chemotaxis','Free Nav.', 'Chemotaxis','Free Nav.'});
+set(gca, 'XTick', [2,6], 'XTickLabel', {'Chemotaxis','Free Navigation'});
 ylabel(yLabel);
 
 % Adjust figure properties
+
 grid on;
+set(gca,'XGrid','off')
 set(gcf, 'Units', 'inches');
 set(gcf, 'Position', [2, 2, 10, 6]); % Adjust width and height as needed
 set(gca, 'FontName', fontNameFigure, 'FontSize', fontSizeFigure);
